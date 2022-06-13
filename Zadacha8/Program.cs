f98bb8472765fc38b45f2879720affa73c58675b
Lakosta <8bit92@gmail.com>
2022-06-13 18:22:50 +0300

    Add Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, */
+
+Console.Clear();
+
+Console.WriteLine("Введите число ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int sum = 1;
+
+Console.WriteLine("Чётные числа в заданном диапазоне: ");
+
+while (sum < N)
+{
+    if (sum % 2 == 0)
+    {
+        Console.WriteLine(sum + "");
+    }
+    sum++;
+}
